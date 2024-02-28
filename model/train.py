c3e5bb8fc55d93cf5fcad6befa8331453ca6c588
Akash <Akash>
2024-02-28 15:07:33 +0000

    remove aws yaml, modularise, add to do.

--- model/train.py
+++ model/train.py
@@ -3,26 +3,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from fastapi import FastAPI
-import uvicorn
-
-import os
-
-
-# Change the current working directory
-os.chdir('./')
+from model.data_process import get_data
 
 app = FastAPI()
 
-fruits = pd.read_table('data/fruit_data_with_colors.txt')
+d = get_data()
+d.transform()
+X_train, X_test, y_train,y_test = d.get()
 
-feature_names = ['mass', 'width', 'height', 'color_score']
-X = fruits[feature_names]
-y = fruits['fruit_label']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
 
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
