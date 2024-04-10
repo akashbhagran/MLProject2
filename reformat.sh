31d333503e2f37249eac801940a8946c96e4fa92
Akash <Akash>
2024-04-10 13:36:02 +0100

    added reformatting for other folders

--- reformat.sh
+++ reformat.sh
@@ -3,4 +3,10 @@
 for f in model/*.py; do python -m black "$f"; done
 
 for f in unittests/*.py; do python -m black "$f"; done
- 
+
+for f in run/*.py; do python -m black "$f"; done
+
+for f in simulate/*.py; do python -m black "$f"; done
+
+for f in train/*.py; do python -m black "$f"; done
+
