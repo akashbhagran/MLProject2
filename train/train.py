dcdc4ade8e71c170bb08d4bb1dd5626f287c8586
Akash <Akash>
2024-05-28 19:25:41 +0100

    change paths again

--- train/train.py
+++ train/train.py
@@ -31,7 +31,7 @@ with open("metrics.txt", "w") as outfile:
 
 cm = confusion_matrix(y_test,y_pred)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm).plot()
-disp.figure_.savefig('train/confusion_matrix.png')
+disp.figure_.savefig('confusion_matrix.png')
 
 current_dateTime = datetime.now()
 name = "LogRegFruits"
