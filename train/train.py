df0058f37a7ffc92a8fed66f7f6833ac43571f3e
Akash <Akash>
2024-05-28 17:13:54 +0100

    added plot

--- train/train.py
+++ train/train.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import mlflow
 from mlflow.models import infer_signature
 import pickle
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 mlflow.set_tracking_uri("http://localhost:5000")
 
@@ -19,6 +20,7 @@ with open("config.json") as f:
 
 logreg = LogisticRegression(penalty = config['penalty'], fit_intercept=config['fit_intercept'], C = config['C'])
 logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
 
 pickle.dump(logreg, open('model.sav', 'wb'))
 logreg = pickle.load(open('model.sav', 'rb'))
@@ -27,11 +29,13 @@ acc = logreg.score(X_test,y_test)
 with open("metrics.txt", "w") as outfile:
     outfile.write("Accuracy: " + str(acc))
 
+cm = confusion_matrix(y_test,y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.figure_.savefig('confusion_matrix.png')
+
 current_dateTime = datetime.now()
 name = "LogRegFruits"
-
 mlflow.set_experiment("Run1")
-
 signature = infer_signature(X_train, logreg.predict(X_train))
 
 if __name__ == '__main__':
